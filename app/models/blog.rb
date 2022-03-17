470d3c337d9d10f2e2caede5d8554fc5f330fc2f
Don Forrest <forrestusbal@gmail.com>
2022-03-17 16:32:40 +0800

    Install active_storage, blog has one image

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,11 @@
 class Blog < ApplicationRecord
   validates :title, :body, presence: true
 
+  has_one_attached :image
+
   scope :by_recently_updated, -> { order(updated_at: :desc) }
+
+  def has_image?
+    image.attached?
+  end
 end
