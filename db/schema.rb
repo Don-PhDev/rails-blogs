fe6943a6678eb82410130dff829de972e8e763e0
Don Forrest <forrestusbal@gmail.com>
2022-03-19 13:29:11 +0800

    Correct migration

--- db/schema.rb
+++ db/schema.rb
@@ -45,6 +45,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_19_050805) do
   create_table "blogs", force: :cascade do |t|
     t.string "title", null: false
     t.text "body", null: false
+    t.integer "views", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
@@ -62,6 +63,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_19_050805) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -69,7 +71,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_19_050805) do
     t.datetime "remember_created_at", precision: nil
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
