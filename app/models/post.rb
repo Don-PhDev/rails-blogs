fb7851bbd341e49487ad114003496c65c28b6744
Don Forrest <forrestusbal@gmail.com>
2022-03-20 23:07:56 +0800

    Add table comments, model associations, and comment routes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,10 @@
 class Post < ApplicationRecord
   validates :title, :body, presence: true
 
-  has_many_attached :images
-
   belongs_to :user
+
+  has_many_attached :images
+  has_many :comments
 
   scope :by_recently_updated, -> { order(updated_at: :desc) }
 
