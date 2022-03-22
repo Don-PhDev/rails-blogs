7e0af05079ada2931b9f8d54d5b8dd6579dbd12a
Don Forrest <forrestusbal@gmail.com>
2022-03-22 19:02:49 +0800

    Include helper DateHelper and define commented_time_ago

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
+  include ActionView::Helpers::DateHelper
+
   belongs_to :post
   belongs_to :user
   belongs_to :parent, class_name: "Comment", optional: true
@@ -6,4 +8,13 @@ class Comment < ApplicationRecord
   has_many :comments, foreign_key: :parent_id
 
   validates :content, presence: true
+
+  def commented_time_ago
+    if self.updated_at - self.created_at > 1
+      "edited " + time_ago_in_words(self.updated_at) + " ago"
+    else
+      # "created " + distance_of_time_in_words(Time.now, self.created_at.to_time) + " ago"
+      time_ago_in_words(self.created_at) + " ago"
+    end
+  end
 end
